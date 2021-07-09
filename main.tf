c004b7c3ae9b7d0efcc74ae823fe25d13a79594e
tmax-jimin-kwon <82425297+tmax-jimin-kwon@users.noreply.github.com>
2021-07-09 17:04:18 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,6 +63,6 @@ resource "aws_lb_listener" "app" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.test.arn
   }
 }
