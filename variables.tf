47ad52d924d96516f1af871e6a50de6c456e6f95
tmax-jimin-kwon <82425297+tmax-jimin-kwon@users.noreply.github.com>
2021-07-14 17:08:11 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The region Terraform deploys your instances"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
